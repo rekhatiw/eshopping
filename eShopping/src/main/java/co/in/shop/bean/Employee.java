7baa92ab6c10e82a6cfc76671b8133569d5cda98
=rekhatiw <=rek.pandey@gmail.com>
2024-06-29 13:17:20 +0530

    changes 29-06-2024

--- eShopping/src/main/java/co/in/shop/bean/Employee.java
+++ eShopping/src/main/java/co/in/shop/bean/Employee.java
@@ -2,5 +2,34 @@ package co.in.shop.bean;
 import javax.persistence.*;
 
 public class Employee {
+	
+	private String firstName;
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getSecondName() {
+		return secondName;
+	}
+	public void setSecondName(String secondName) {
+		this.secondName = secondName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	private String secondName;
+	private String email;
+	private String address;
 
 }
